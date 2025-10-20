12e898a401b66d51b42f70a8d34a16ed487988a1
seungjun <sj7934@gmail.com>
2025-10-21 01:53:05 +0900

    테스트 코드 수정

--- crawling/crawling_test.py
+++ crawling/crawling_test.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.common.by import By
 import time
 from selenium import webdriver
+from elasticsearch import Elasticsearch
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from pymongo import MongoClient
@@ -8,7 +9,6 @@ from dotenv import load_dotenv
 import os
 import datetime
 import hashlib
-
 # .env 파일 불러오기
 load_dotenv()
 
@@ -20,13 +20,15 @@ DB_NAME = os.getenv("DB_NAME")
 client = MongoClient(MONGO_URI)
 db = client[DB_NAME]
 
-#크롤링
+CHROMEDRIVER_PATH = os.getenv("CHROMEDRIVER_PATH", "chromedriver/chromedriver.exe")
+service = Service(CHROMEDRIVER_PATH)
+#동적 크롤링
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 chrome_options = Options()
 chrome_options.add_argument("--proxy-server=socks5://127.0.0.1:9150")
-service = Service(
-    r"C:\Users\Seung Jun\Desktop\취업 준비 자료\구름 공부 자료\Semi-Project\Semi-Project-darkweb-\chromedriver\chromedriver.exe"
-)
+
+service = Service(CHROMEDRIVER_PATH)
+
 driver = webdriver.Chrome(service=service, options=chrome_options)
 driver.get("http://z3wqggtxft7id3ibr7srivv5gjof5fwg76slewnzwwakjuf3nlhukdid.onion/blog")
 time.sleep(1)
@@ -38,29 +40,32 @@ collection = db['leaked_data']
 try:
     for i in contents:
         try:
-            content = i.text.strip()
-            hash_id = hashlib.md5(content.encode('utf-8')).hexdigest()
             company_names = i.find_element(By.CLASS_NAME, "list-publication__name").text.strip()
+            hash_id = hashlib.md5(company_names.encode('utf-8')).hexdigest()
             company_url = i.find_element(By.CSS_SELECTOR, "p.publication-addictional__row a.addiction-row__text.addictional-row__link").text.strip()
-            dercription = i.find_element(By.CLASS_NAME, "list-publication__description").text.strip()
+            description = i.find_element(By.CLASS_NAME, "list-publication__description").text.strip()
             publication_date = i.find_element(By.CLASS_NAME, "publication-footer__date").text.strip()
             
             doc = {
                 "_id": "dragonforce_" + hash_id,
                 "company_name": company_names,
                 "company_url" : company_url,
-                "description" : dercription,
+                "description" : description,
                 "publication_date" : publication_date,     
-                "scraped_at": datetime.datetime.utcnow()
+                "scraped_time": datetime.datetime.utcnow()
                 }
             
         except Exception as e:
             print(f"크롤링 중 오류 발생: {e}")
 
-        #중복 id 방지 & 새로운 데이터가 있으면 업데이트
-        collection.insert_one(doc)
-        if "_id" in doc:
-            del doc["_id"]
+        collection.update_one(
+            {"_id": doc["_id"]},  
+            {"$set": doc},       
+            upsert=True           
+            )
     print(" -> Saved to MongoDB")
 except Exception as e:
     print(" -> MongoDB insert error:", e)
+
+
+
