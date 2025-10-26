df3743925d4741b6cb413512b53cb2e57ffd4519
seungjun <sj7934@gmail.com>
2025-10-27 01:47:23 +0900

    코드 수정

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-test
+from pymongo import MongoClient
+
+client = MongoClient("REMOVED_MONGODB_URI")
+db = client["darkweb"]
+collection = db["leaked_data"]
+
+result = collection.delete_many({"_id": {"$regex": "^gunra_"}})
+print(f"{result.deleted_count}개의 문서 삭제 완료 ✅")
