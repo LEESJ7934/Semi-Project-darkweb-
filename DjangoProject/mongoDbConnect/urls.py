fab88242a1c7bf81cb3fdbb46fd7c9b3fcd412be
woojong <dnwhd0112@naver.com>
2025-11-28 10:51:29 +0900

    로그인 페이지 추가

--- DjangoProject/mongoDbConnect/urls.py
+++ DjangoProject/mongoDbConnect/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("", views.latest_data_table, name="latest_data_table"),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
 ]
